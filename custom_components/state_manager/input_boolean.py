6b89f25b8d40b9db0033832e134cd58aa4b9cd97
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-11 16:25:38 +1000

    input boolean updates

--- custom_components/state_manager/input_boolean.py
+++ custom_components/state_manager/input_boolean.py
@@ -8,6 +8,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class StateInputBoolean(InputBoolean):
     def __init__(self, manager):
         self.manager = manager
+        self._state = False  # Initial state off
 
     @property
     def unique_id(self):
@@ -19,4 +20,28 @@ class StateInputBoolean(InputBoolean):
         """Return the name of the input_boolean."""
         return f"{self.manager.name} Input Boolean"
 
-    # Implement other required methods...
+    @property
+    def is_on(self):
+        """Return the current state of the input_boolean."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Turn the input_boolean on."""
+        self._state = True
+        # Update state manager based on turn on
+        self.manager.on_turn_on()
+
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """Turn the input_boolean off."""
+        self._state = False
+        # Update state manager based on turn off
+        self.manager.on_turn_off()
+
+        self.schedule_update_ha_state()
+
+    def update(self):
+        """Update the state of the input_boolean."""
+        self._state = self.manager.is_enabled()
+        self.schedule_update_ha_state()
