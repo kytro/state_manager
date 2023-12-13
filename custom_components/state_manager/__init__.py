dd413afe0d517bb4175d103180dc338acf75dec9
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-13 11:41:26 +1000

    Add more logging

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from homeassistant.helpers.entity import DeviceInfo, Entity
 from homeassistant.helpers.device_registry import async_get as get_dev_reg
+from homeassistant.helpers.entity_registry import async_get as get_ent_reg
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'state_manager'
@@ -44,6 +45,20 @@ class StateManager(Entity):
             model=self._device_info.model,
         )
 
+        # Create a new entity for the device
+        entity_id = f"{DOMAIN}.{self._id}"
+        self.hass.states.async_set(entity_id, 'off')
+
+        # Add the entity to the entity registry
+        ent_reg = get_ent_reg(self.hass)
+        ent_reg.async_get_or_create(
+            domain=DOMAIN,
+            platform=DOMAIN,
+            unique_id=self.unique_id,
+            device_id=self._id,
+            config_entry_id=self.unique_id,
+            name=self._name,
+        )
 
     async def async_update(self):
         _LOGGER.info("Updating state")
@@ -51,8 +66,10 @@ class StateManager(Entity):
 
 async def async_setup_entry(hass, config_entry):
     _LOGGER.info("Setting up state_manager")
-    # Set up your component here
     devices = config_entry.data['devices']
     for device in devices:
+        _LOGGER.info(f"Creating device: {device['id']}")
         hass.states.async_set(device['id'], 'off')
+        entity = StateManager(hass, device['name'], device['id'])
+        hass.add_job(entity.async_added_to_hass)
     return True
