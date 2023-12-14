896a5dabde730ce5494caf635b94d69dd9501348
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-14 19:23:39 +1000

    ai fixes

--- custom_components/state_manager/sensor.py
+++ custom_components/state_manager/sensor.py
@@ -1,9 +1,16 @@
 from homeassistant.helpers.entity import Entity
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    async_add_entities([StateManagerSensor()])
+    # Fix: call the correct setup method
+    if "state_manager" in config:
+        async_add_entities([StateManagerSensor(config["state_manager"])])
+    return True
 
 class StateManagerSensor(Entity):
+    def __init__(self, config):
+        self._config = config
+
     @property
     def state(self):
-        return "boo"
+        # Use data from config if available
+        return self._config.get("state", "boo")
