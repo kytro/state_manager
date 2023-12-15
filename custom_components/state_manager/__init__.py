83b8ffb7bf3cd7ee1e39291ea378cc9bdf5326d6
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-15 12:45:39 +1000

    Try to add to device registry

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,5 +1,15 @@
+from homeassistant.helpers import device_registry as dr
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
     hass.data[DOMAIN] = entry.data['name']
+
+    device_registry = await dr.async_get_registry(hass)
+
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        identifiers={(DOMAIN, entry.data['name'])},
+        name=entry.data['name'],
+    )
+
     return True
