b7cf6be3e26ec8b1fc2a09c5ece9bc9475e04d4d
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-15 21:05:11 +1000

    eh

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -9,17 +9,15 @@ async def async_setup_entry(hass, entry):
 
     for device in device_registry.devices.values():
         if entry.entry_id in device.config_entries:
-            device_info = device_registry.async_get_or_create(
+            device_info = {
+                "identifiers": {(DOMAIN, device.id)},
+                "name": f"{device.name} Enabled",
+                # ... additional device information ...
+            }
+            device_registry.async_get_or_create(
                 identifiers={(DOMAIN, device.id)},
                 name=device.name,
-                device_info={
-                    "identifiers": {(DOMAIN, device.id)},
-                    "name": f"{device.name} Enabled",
-                    "manufacturer": "Your Manufacturer",
-                    "model": "Your Model",
-                    "sw_version": "1.0",
-                    "via_device": (DOMAIN, device.id),
-                },
+                device_info=device_info,
             )
             input_boolean = StateManagerEnabled(hass, device)
             hass.add_job(input_boolean.update_state, 'off')
