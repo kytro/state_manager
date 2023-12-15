3e011e90bd570a84167384048a57fb503e29c464
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-15 12:52:49 +1000

    Device async remove

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -4,7 +4,7 @@ from .const import DOMAIN
 async def async_setup_entry(hass, entry):
     hass.data[DOMAIN] = entry.data['name']
 
-    device_registry = await dr.async_get(hass)
+    device_registry = dr.async_get(hass)
 
     device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
