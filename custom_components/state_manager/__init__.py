8c73a5e2e6f89aa8e2b68d7aa9a5cb5c01835843
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-15 15:51:04 +1000

    Associate boolean

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,13 +1,13 @@
 from homeassistant.helpers import device_registry as dr
 from .const import DOMAIN
-from .input_boolean import create_input_boolean
+from .input_boolean import StateManagerEnabled
 
 async def async_setup_entry(hass, entry):
     hass.data[DOMAIN] = entry.data['name']
 
     device_registry = dr.async_get(hass)
 
-    device_registry.async_get_or_create(
+    device_info = device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
         identifiers={(DOMAIN, entry.data['name'])},
         name=entry.data['name'],
@@ -16,6 +16,7 @@ async def async_setup_entry(hass, entry):
     # Create an input boolean for each device
     for device in device_registry.devices.values():
         if entry.entry_id in device.config_entries:
-            create_input_boolean(hass, device)
+            input_boolean = StateManagerEnabled(hass, device)
+            hass.add_job(input_boolean.update_state, 'off')
 
     return True
--- custom_components/state_manager/input_boolean.py
+++ custom_components/state_manager/input_boolean.py
@@ -1,5 +1,34 @@
 from homeassistant.components import input_boolean
+from homeassistant.helpers.entity import Entity
+from .const import DOMAIN
 
-def create_input_boolean(hass, device):
-    input_boolean_entity_id = f"input_boolean.{device.name}_enabled"
-    hass.states.async_set(input_boolean_entity_id, 'off', {"friendly_name": f"{device.name} Enabled"})
+class StateManagerEnabled(Entity):
+    def __init__(self, hass, device):
+        self.hass = hass
+        self.device = device
+        self.entity_id = f"input_boolean.{device.name}_enabled"
+        self._state = 'off'
+        self._name = f"{device.name} Enabled"
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.device.id)},
+            "name": self._name,
+            "manufacturer": "Your Manufacturer",
+            "model": "Your Model",
+            "sw_version": "1.0",
+            "via_device": (DOMAIN, self.device.id),
+        }
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def name(self):
+        return self._name
+
+    def update_state(self, state):
+        self._state = state
+        self.async_write_ha_state()
