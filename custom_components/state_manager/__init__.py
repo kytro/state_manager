5bfe78b7a8c5f35045d9fde0fb3fd9738a08b559
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-18 07:32:37 +1000

    Group

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -5,7 +5,7 @@ async def async_setup_entry(hass, entry):
     hass.data[DOMAIN] = entry.data['name']
 
     # Get the entity registry
-    registry = await entity_registry.async_get_registry(hass)
+    registry = entity_registry.async_get(hass)
 
     # Create a new group
     group = registry.entities.get(hass.data[DOMAIN])
