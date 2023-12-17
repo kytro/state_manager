d123df420a5bb5ef0ef8c9623f4038770c3a41cd
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-18 07:47:26 +1000

    Add group

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,11 +1,18 @@
 from homeassistant.helpers import entity_registry
+from homeassistant.components import input_boolean
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
     hass.data[DOMAIN] = entry.data['name']
 
+    # Create an input_boolean
+    input_boolean_entity_id = f"input_boolean.{hass.data[DOMAIN]}_enabled"
+    await hass.services.async_call('input_boolean', 'turn_on', {
+        'entity_id': input_boolean_entity_id
+    })
+
     # Get the entity registry
-    registry = entity_registry.async_get(hass)
+    registry = await entity_registry.async_get(hass)
 
     # Create a new group
     group = registry.entities.get(hass.data[DOMAIN])
@@ -19,5 +26,7 @@ async def async_setup_entry(hass, entry):
             config_entry=entry,
         )
 
-    return True
+    # Add the input_boolean to the group
+    group.entities.append(input_boolean_entity_id)
 
+    return True
--- custom_components/state_manager/input_boolean.py
+++ custom_components/state_manager/input_boolean.py
@@ -1,56 +1,11 @@
-from homeassistant.components import input_boolean
-from homeassistant.helpers.entity import Entity
-from homeassistant.const import CONF_DEVICES
-from homeassistant.helpers import config_validation as cv
-from homeassistant.components.light import PLATFORM_SCHEMA
+from homeassistant.components.input_boolean import InputBoolean
 from .const import DOMAIN
 
-import voluptuous as vol
+async def async_setup(hass, config):
+    # Create an input_boolean
+    input_boolean_entity_id = f"input_boolean.{hass.data[DOMAIN]}_enabled"
+    await hass.services.async_call('input_boolean', 'turn_on', {
+        'entity_id': input_boolean_entity_id
+    })
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_DEVICES): vol.All(cv.ensure_list, [cv.string]),
-})
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the platform."""
-    devices = config.get(CONF_DEVICES)
-    entities = [StateManagerEnabled(device) for device in devices]
-    async_add_entities(entities, True)
-
-class StateManagerEnabled(Entity):
-
-    def __init__(self, hass, device):
-        self.entity_id = f"input_boolean.{device.name}_enabled"
-        self.device = device
-        self.hass = hass
-
-    @property
-    def unique_id(self):
-        return self._attr_unique_id
-
-    @unique_id.setter
-    def unique_id(self, unique_id):
-        self._attr_unique_id = unique_id
-
-    @property
-    def device_info(self):
-        return {
-            "identifiers": {(DOMAIN, self.device.id)},
-            "name": f"{self.device.name} Enabled",
-            "manufacturer": "Your Manufacturer",
-            "model": "Your Model",
-            "sw_version": "1.0",
-            "via_device": (DOMAIN, self.device.id),
-        }
-
-    @property
-    def icon(self):
-        return "mdi:toggle-switch"  # Replace with desired icon
-
-    def update_state(self, state):
-        self._state = state
-        self.async_write_ha_state()
-
-    def configure_entity(self, hass, config_entry, options=None):
-        super().configure_entity(hass, config_entry, options=options)
-        # ... additional configuration based on options (optional) ...
+    return True
