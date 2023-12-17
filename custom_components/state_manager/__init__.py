81143a23e4c0c898b38b0a0978f0566a095fe041
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-18 07:21:11 +1000

    Group

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,17 +1,23 @@
-from homeassistant.helpers import entity_component
+from homeassistant.helpers import entity_registry
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
     hass.data[DOMAIN] = entry.data['name']
 
-    # Create a new group
-    group = entity_component.EntityComponent(
-        hass,
-        DOMAIN,
-        hass.data[DOMAIN]
-    )
+    # Get the entity registry
+    registry = await entity_registry.async_get_registry(hass)
 
-    # Add the group to Home Assistant
-    await group.async_added_to_hass()
+    # Create a new group
+    group = registry.entities.get(hass.data[DOMAIN])
+
+    if group is None:
+        group = registry.async_get_or_create(
+            domain=DOMAIN,
+            platform='group',
+            unique_id=hass.data[DOMAIN],
+            suggested_object_id=hass.data[DOMAIN],
+            config_entry=entry,
+        )
 
     return True
+
