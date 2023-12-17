544ab9fd351ec87566de8e7234e42e2c6dac8997
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-18 08:10:17 +1000

    Input boolean

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -7,7 +7,7 @@ async def async_setup_entry(hass, entry):
 
     # Create an input_boolean
     input_boolean_entity_id = f"input_boolean.{hass.data[DOMAIN]}_enabled"
-    await hass.services.async_call('input_boolean', 'turn_on', {
+    await hass.services.async_call('input_boolean', 'toggle', {
         'entity_id': input_boolean_entity_id
     })
 
--- custom_components/state_manager/input_boolean.py
+++ custom_components/state_manager/input_boolean.py
@@ -1,32 +1,10 @@
-from homeassistant.helpers import entity_registry
-from homeassistant.components import input_boolean
-from .const import DOMAIN
+from homeassistant.components.input_boolean import InputBoolean
 
-async def async_setup_entry(hass, entry):
-    hass.data[DOMAIN] = entry.data['name']
-
+async def async_setup(hass, config):
     # Create an input_boolean
     input_boolean_entity_id = f"input_boolean.{hass.data[DOMAIN]}_enabled"
     await hass.services.async_call('input_boolean', 'toggle', {
         'entity_id': input_boolean_entity_id
     })
-
-    # Get the entity registry
-    registry = await entity_registry.async_get(hass)
-
-    # Create a new group
-    group = registry.entities.get(hass.data[DOMAIN])
-
-    if group is None:
-        group = registry.async_get_or_create(
-            domain=DOMAIN,
-            platform='group',
-            unique_id=hass.data[DOMAIN],
-            suggested_object_id=hass.data[DOMAIN],
-            config_entry=entry,
-        )
-
-    # Add the input_boolean to the group
-    group.entities.append(input_boolean_entity_id)
 
     return True
