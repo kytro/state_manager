85fbd82b7be907a9e98c243d8cddf91731b29040
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-18 10:03:05 +1000

    Attmep x

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,22 +1,32 @@
 from homeassistant.helpers import device_registry as dr
 from .const import DOMAIN
 from .input_boolean import StateManagerEnabled
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
-async def async_setup_entry(hass, entry):
+from homeassistant.const import (
+    Platform,
+)
+
+PLATFORMS: list[Platform] = [Platform.SWITCH]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN] = entry.data['name']
 
     device_registry = dr.async_get(hass)
 
-    for device in device_registry.devices.values():
-        if entry.entry_id in device.config_entries:
-            # Get or create the device
-            device = device_registry.async_get_or_create(
-                #config_entry_id=entry.entry_id,
-                identifiers={(DOMAIN, device.id)},
-                name=device.name,
-            )
-            #input_boolean = StateManagerEnabled(hass, device)
-            #input_boolean.unique_id = entry.entry_id
-            #hass.add_job(input_boolean.update_state, 'off')
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id]
+
+    #for device in device_registry.devices.values():
+    #    if entry.entry_id in device.config_entries:
+    #        # Get or create the device
+    #        device = device_registry.async_get_or_create(
+    #            #config_entry_id=entry.entry_id,
+    #            identifiers={(DOMAIN, device.id)},
+    #            name=device.name,
+    #        )
+    #        #input_boolean = StateManagerEnabled(hass, device)
+    #        #input_boolean.unique_id = entry.entry_id
+    #        #hass.add_job(input_boolean.update_state, 'off')
 
     return True
