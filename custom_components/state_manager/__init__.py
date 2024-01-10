71c65766c2976355cf4a8b2be21aedfaffac1c0c
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-11 09:49:26 +1000

    dictionary

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -3,16 +3,20 @@ from homeassistant.helpers import discovery
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
-from homeassistant.const import CONF_DEVICES
+from homeassistant.const import CONF_DEVICES, CONF_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "state_manager"
 
+DEVICE_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string,
+})
+
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
-            vol.All(dict, {cv.string: cv.string}),
+            vol.All(dict, {cv.string: DEVICE_SCHEMA}),
         )
     },
     extra=vol.ALLOW_EXTRA,
@@ -30,10 +34,10 @@ def setup(hass, config):
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
 
-    for unique_id in devices:
+    for unique_id, device_config in devices.items():
         """Adding device: {unique_id}..."""
         _LOGGER.info("Adding device: %s...", unique_id)
-        name = unique_id
+        name = device_config[CONF_NAME]
         manager = StateManager(name, unique_id)
         hass.data[DOMAIN][unique_id] = manager
         discovery.load_platform(
