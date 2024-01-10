7d5d94f811b931e4c592762d1e615d1bc32a578e
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-10 13:34:57 +1000

    config

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,21 +1,25 @@
 from .const import DOMAIN
 import logging
 import voluptuous as vol
-
-from homeassistant.helpers.config_validation import (  # Updated import
-    PLATFORM_SCHEMA,
-    PLATFORM_SCHEMA_BASE,
-)
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import Entity
 from . import switch
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(  # Use PLATFORM_SCHEMA for validation
+CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required("name"): vol.All(str, vol.Length(min=1)),
-        vol.Required("unique_id"): str,
-    }
+        DOMAIN: vol.Schema(
+            {
+                cv.string: vol.Schema(
+                    {
+                        vol.Required("unique_id"): cv.string,
+                    }
+                )
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
 )
 
 async def async_setup(hass, config):
@@ -27,6 +31,8 @@ async def async_setup(hass, config):
         switch_entity = switch.create_switch_entity(hass, name, unique_id)
 
         hass.data[DOMAIN][unique_id] = switch_entity
+
+    return True
 
     return True
 
