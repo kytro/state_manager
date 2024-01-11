659332b795a947ce7717d03458dd8f2145cfd96a
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-11 16:40:53 +1000

    parameters

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -3,7 +3,7 @@ from .const import DOMAIN
 
 async def async_setup(hass, config):
 
-    input_boolean = InputBoolean("porch_light_managed_enabled", "Porch Light Managed Enabled", initial=True)
+    input_boolean = InputBoolean("porch_light_managed_enabled", "Porch Light Managed Enabled")
     
     hass.async_create_task(hass.helpers.entity_component.async_add_entities([input_boolean]))   
 
