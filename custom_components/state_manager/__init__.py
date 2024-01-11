cddb5344c3873babe569c159af610ab9a4fdac10
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-11 12:24:26 +1000

    Input boolean

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,4 +1,13 @@
+from homeassistant.components.input_boolean import async_setup as async_setup_input_boolean
 from .const import DOMAIN
 
 async def async_setup(hass, config):
+
+    input_boolean_config = {
+        "name": "My Input Boolean",
+        "initial": True,
+    }
+    
+    await async_setup_input_boolean(hass, input_boolean_config)    
+
     return True
