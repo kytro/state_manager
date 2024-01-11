d6509a87d02b46881e85e0775b4d21fd05175be6
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-11 11:25:59 +1000

    input boolean

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,5 +1,4 @@
 import logging
-
 from homeassistant.components.input_boolean import (
     DOMAIN as INPUT_BOOLEAN_DOMAIN,
     InputBoolean,
@@ -10,18 +9,12 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 _LOGGER = logging.getLogger(__name__)
 
-async
- 
-def
- 
-async_setup_entry(
+async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-
-    
-"""Set up state_manager from a config entry."""
+    """Set up state_manager from a config entry."""
     name = config_entry.data["name"]
     enabled_name = f"{name}_enabled"
 
@@ -31,7 +24,6 @@ async_setup_entry(
     async_add_entities([input_boolean])
 
     _LOGGER.info("State Manager input boolean '%s' created", enabled_name)
-
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload a config entry."""
