acf5d30a24370a8099959291dcd488972c39bdb7
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-12 19:08:21 +1000

    Basis

---
+++ custom_components/auxiliary/__init__.py
@@ -0,0 +1,41 @@
+rom logging import getLogger
+
+from voluptuous import Required, Schema
+
+from homeassistant.components.automation import EVENT_AUTOMATION_RELOADED
+from homeassistant.const import CONF_ENTITY_ID, CONF_STATE, EVENT_HOMEASSISTANT_STARTED
+from homeassistant.core import HomeAssistant, Event
+from homeassistant.helpers.config_validation import string
+from .helper import create_automations, create_entities_and_automations, CONFIG_INPUT_BOOLEAN, COMPONENT_INPUT_BOOLEAN, \
+    CONFIG_INPUT_DATETIME, COMPONENT_INPUT_DATETIME, CONFIG_INPUT_NUMBER, COMPONENT_INPUT_NUMBER, CONFIG_INPUT_TEXT, \
+    COMPONENT_INPUT_TEXT, CONFIG_TIMER, COMPONENT_TIMER
+
+DOMAIN = 'auxiliary'
+
+SCHEMA_SET_STATE = Schema(
+    {
+        Required(CONF_ENTITY_ID): string,
+        Required(CONF_STATE): string
+    }
+)
+
+_LOGGER = getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    CONFIG_INPUT_BOOLEAN.update(config.get(COMPONENT_INPUT_BOOLEAN, {}))
+    CONFIG_INPUT_DATETIME.update(config.get(COMPONENT_INPUT_DATETIME, {}))
+    CONFIG_INPUT_NUMBER.update(config.get(COMPONENT_INPUT_NUMBER, {}))
+    CONFIG_INPUT_TEXT.update(config.get(COMPONENT_INPUT_TEXT, {}))
+    CONFIG_TIMER.update(config.get(COMPONENT_TIMER, {}))
+
+    async def handle_home_assistant_started_event(event: Event):
+        await create_entities_and_automations(hass)
+
+    async def handle_automation_reload_event(event: Event):
+        await create_automations(hass)
+
+    hass.bus.async_listen(EVENT_HOMEASSISTANT_STARTED, handle_home_assistant_started_event)
+    hass.bus.async_listen(EVENT_AUTOMATION_RELOADED, handle_automation_reload_event)
+
+    return True
--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,72 +1,33 @@
-"""The state_manager component."""
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-from homeassistant.const import CONF_FRIENDLY_NAME
-from homeassistant.components.input_boolean import DOMAIN as INPUT_BOOLEAN
-from homeassistant.helpers.entity_platform import async_get_platforms
-from homeassistant.util import slugify
-from homeassistant.components.input_boolean import async_setup as setup_input_boolean
+import logging
+from datetime import timedelta
 
+from homeassistant.helpers.entity_component import EntityComponent
+from .const import DOMAIN, SERVICE_DONE
 
-DOMAIN = "state_manager"
+ENTITY_ID_FORMAT = DOMAIN + ".{}"
 
-COMPONENT_INPUT_BOOLEAN = 'input_boolean'
+_LOGGER = logging.getLogger(__name__)
 
-CONFIG_INPUT_BOOLEAN = {}
+SCAN_INTERVAL = timedelta(seconds=86400)
 
-ENTITY_SCHEMA = vol.Schema(
-    {
-        vol.Optional(CONF_FRIENDLY_NAME): cv.string,
-    }
-)
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {cv.slug: ENTITY_SCHEMA}
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
 
 async def async_setup(hass, config):
-    """Set up the state_manager component."""
-    conf = config[DOMAIN]
-
-    # Ensure the platform is loaded
-    hass.async_create_task(
-        hass.helpers.discovery.async_load_platform(INPUT_BOOLEAN, DOMAIN, {}, config)
+    component = hass.data[DOMAIN] = EntityComponent(
+        _LOGGER, DOMAIN, hass, SCAN_INTERVAL
     )
 
-    # Get the input_boolean platform
-    platform = None
-    for p in async_get_platforms(hass, INPUT_BOOLEAN):
-        if p.domain == INPUT_BOOLEAN:
-            platform = p
-            break
+    await component.async_setup(config)
 
-    # Create the input_boolean for each entity in the configuration
-    for entity_id, entity_conf in conf.items():
-        await create_input_boolean(f"{entity_id}_enabled")
-
-    await setup_input_boolean(hass, {COMPONENT_INPUT_BOOLEAN: CONFIG_INPUT_BOOLEAN})
+    component.async_register_entity_service(
+        SERVICE_DONE, {}, "done",
+    )
 
     return True
 
 
-async def create_input_boolean(name: str, icon=None) -> str:
-    data = {
-        'name': name
-    }
+async def async_setup_entry(hass, entry):
+    return await hass.data[DOMAIN].async_setup_entry(entry)
 
-    if icon:
-        data['icon'] = icon
 
-    internal_name = slugify(name)
-
-    CONFIG_INPUT_BOOLEAN[internal_name] = data
-
-    global CUSTOM_INPUT_BOOLEAN
-    CUSTOM_INPUT_BOOLEAN = True
-
-    return 'input_boolean.{}'.format(internal_name)
+async def async_unload_entry(hass, entry):
+    return await hass.data[DOMAIN].async_unload_entry(entry
