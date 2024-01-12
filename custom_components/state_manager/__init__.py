e99bd2bfa779e6d46f91db5484cc104f01febfae
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-12 12:30:39 +1000

    via panel

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,32 +1,43 @@
 import logging
+from asyncio import async_create_task
+
 from .const import DOMAIN
-from homeassistant.components.input_boolean import async_setup_platform
-from homeassistant.const import CONF_NAME, CONF_ICON
-from homeassistant.helpers.entity import async_generate_entity_id
-from homeassistant.components.input_boolean import DOMAIN as INPUT_BOOLEAN_DOMAIN
-from homeassistant.helpers.entity_component import EntityComponent
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
-    # Create a new EntityComponent named 'input_boolean'
-    component = EntityComponent(_LOGGER, INPUT_BOOLEAN_DOMAIN, hass)
+    _LOGGER.info("Setting up Porch Light Manager component")
 
-    # Define the configuration for the input boolean
-    switch_config = {
-        'name': "Porch Light Manager Enabled",
-        'initial': False,
-        'icon': "mdi:lightbulb",
-    }
+    try:
+        await async_create_task(hass.components.frontend.async_register_built_in_panel(
+            "iframe",
+            "porch_light_manager",
+            "Porch Light Manager",
+            "/api/hassio/app/entrypoint.js",
+            {"entrypoint": "entrypoint.js"},
+        ))
 
-    # Create a new input boolean entity
-    entity = await component.async_add_entity(switch_config)
+        await async_create_task( hass.services.async_register(DOMAIN, "set_porch_light_manager_enabled", set_porch_light_manager_enabled))
 
-    # If the entity was created successfully, set its state
-    if entity:
-        hass.states.async_set(entity.entity_id, 'off', {
-            'friendly_name': switch_config['name'],
-            'icon': switch_config['icon'],
-        })
+        # Create the input boolean entity
+        hass.states.async_set("input_boolean.porch_light_manager_enabled", "off")
+        _LOGGER.info("Input boolean 'porch_light_manager_enabled' created successfully")
 
-    return True
+        return True
+
+    except Exception as e:
+        _LOGGER.error("Failed to set up Porch Light Manager component: %s", e)
+        return False
+    
+async def set_porch_light_manager_enabled(call):
+    """Service to set the porch light manager enabled state."""
+    # Get the desired state from the service call data
+    enabled = call.data.get("enabled", True)
+
+    try:
+        # Set the input boolean state
+        hass.states.async_set("input_boolean.porch_light_manager_enabled", str(enabled))
+
+        _LOGGER.info("Porch light manager enabled state set to: %s", enabled)
+    except Exception as e:
+        _LOGGER.error("Failed to set porch light manager enabled state: %s", e)
