c54294d4ac220a08c2429adf892d0a7276748ff2
Matt Creasey <matt.creasey@imosphere.co.uk>
2024-01-12 14:43:58 +1000

    config

--- custom_components/state_manager/__init__.py
+++ custom_components/state_manager/__init__.py
@@ -1,17 +1,22 @@
 """The state_manager component."""
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from homeassistant.const import CONF_NAME
+from homeassistant.const import CONF_NAME, CONF_FRIENDLY_NAME
 from homeassistant.components.input_boolean import DOMAIN as INPUT_BOOLEAN
 
 DOMAIN = "state_manager"
 
+ENTITY_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_FRIENDLY_NAME): cv.string,
+    }
+)
+
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_NAME): cv.string,
-            }
+            {cv.slug: ENTITY_SCHEMA}
         )
     },
     extra=vol.ALLOW_EXTRA,
@@ -26,9 +31,12 @@ async def async_setup(hass, config):
         hass.helpers.discovery.async_load_platform(INPUT_BOOLEAN, DOMAIN, {}, config)
     )
 
-    # Create the input_boolean
-    hass.states.async_set(
-        f"{INPUT_BOOLEAN}.{conf[CONF_NAME]}", "off", {"friendly_name": conf[CONF_NAME]}
-    )
+    # Create the input_boolean for each entity in the configuration
+    for entity_id, entity_conf in conf.items():
+        hass.states.async_set(
+            f"{INPUT_BOOLEAN}.{entity_id}_enabled",
+            "off",
+            {"friendly_name": f"{entity_conf.get(CONF_FRIENDLY_NAME, entity_id)} Enabled"}
+        )
 
     return True
